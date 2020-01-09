0cd71a9980ef596ca13979badebc5a72205636fc
Saqib <>
2020-01-09 08:02:48 -0500

    Add react-native-paper and make necessary adjustments; add new Well-being graphic assets

--- wellBeingCheck/App.tsx
+++ wellBeingCheck/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-native-paper';
+import App from './src';
+import { theme } from './src/core/theme';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+const Main = () => (
+  <Provider theme={theme}>
+    <App />
+  </Provider>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Main;
---
+++ wellBeingCheck/src/screens/HomeScreen.tsx
@@ -0,0 +1,34 @@
+import React, { memo } from 'react';
+import Background from '../components/Background';
+import Logo from '../components/Logo';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Paragraph from '../components/Paragraph';
+import { Navigation } from '../types';
+
+type Props = {
+  navigation: Navigation;
+};
+
+const HomeScreen = ({ navigation, theme }: Props) => (
+
+  <Background>
+    <Logo />
+    <Header>Well-Being Check</Header>
+
+    <Paragraph>
+      Statistics Canada - Well Being Survey
+    </Paragraph>
+    <Button mode="contained" onPress={() => navigation.navigate('LoginScreen')}>
+      Login
+    </Button>
+    <Button
+      mode="outlined"
+      onPress={() => navigation.navigate('RegisterScreen')}
+    >
+      Sign Up
+    </Button>
+  </Background>
+);
+
+export default memo(HomeScreen);
