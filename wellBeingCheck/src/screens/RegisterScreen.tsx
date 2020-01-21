ab6d8b1adaa2d9707056391ddee418b4cec875de
unrealhound <armonkolaei@gmail.com>
2020-01-21 11:50:50 -0500

    init changes to class

--- wellBeingCheck/src/screens/RegisterScreen.tsx
+++ wellBeingCheck/src/screens/RegisterScreen.tsx
@@ -17,93 +17,105 @@ import {
   securityAnswerValidator,
 } from '../core/utils';
 
-type Props = {
-  navigation: Navigation;
-};
+type RegisterState = {
+  password: string,
+  passwordConfirm: string,
+  securityQuestion: string,
+  securityAnswer: string,
+}
 
-const RegisterScreen = ({ navigation }: Props) => {
-  const [password, setPassword] = useState({ value: '', error: '' });
-  const [passwordConfirm, setPasswordConfirm] = useState({ value: '', error: '' });
-  const [securityQuestion, setSecurityQuestion] = useState({ value: '', error: '' });
-  const [securityAnswer, setSecurityAnswer] = useState({ value: '', error: '' });
+class RegisterScreen extends React.Component<{}, RegisterState> {
+  
+  constructor(RegisterState) {
+    super(RegisterState)
+    this.state = { 
+      password: "",
+      passwordConfirm: "",
+      securityQuestion: "",
+      securityAnswer: "",
+    };
+  }
 
-  const _onSignUpPressed = () => {
-    const passwordError = passwordValidator(password.value);
-    const passwordConfirmError = passwordConfirmValidator(password.value, passwordConfirm.value);
-    const securityQuestionError = securityQuestionValidator(securityQuestion.value);
-    const securityAnswerError = securityAnswerValidator(securityAnswer.value);
+  _onSignUpPressed = () => {
+    console.log("_onSignUpPressed");
+    const passwordError = passwordValidator(this.state.password);
+    const passwordConfirmError = passwordConfirmValidator(this.state.password, this.state.passwordConfirm);
+    const securityQuestionError = securityQuestionValidator(this.state.securityQuestion);
+    const securityAnswerError = securityAnswerValidator(this.state.securityAnswer);
+
+    console.log(passwordError);
 
     if (passwordError || passwordConfirmError || securityQuestionError || securityQuestionError) {
-      setPassword({ ...password, error: passwordError });
-      setPasswordConfirm({ ...passwordConfirm, error: passwordConfirmError });
-      setSecurityQuestion({ ...securityQuestion, error: securityQuestionError });
-      setSecurityAnswer({ ...securityAnswer, error: securityAnswerError });
+      // setPassword({ ...password, error: passwordError });
+      // setPasswordConfirm({ ...passwordConfirm, error: passwordConfirmError });
+      // setSecurityQuestion({ ...securityQuestion, error: securityQuestionError });
+      // setSecurityAnswer({ ...securityAnswer, error: securityAnswerError });
       return;
     }
-
-    navigation.navigate('Dashboard');
   };
 
-  return (
-    <Background>
-      <BackButton goBack={() => navigation.navigate('HomeScreen')} />
-
-      <Logo />
-
-      <Header>Create Account</Header>
-
-      <TextInput
-        label="Paswword"
-        returnKeyType="next"
-        value={password.value}
-        onChangeText={text => setPassword({ value: text, error: '' })}
-        error={!!password.error}
-        errorText={password.error}
-        secureTextEntry={true}
-      />
-
-      <TextInput
-        label="Paswword Confirm"
-        returnKeyType="next"
-        value={passwordConfirm.value}
-        onChangeText={text => setPasswordConfirm({ value: text, error: '' })}
-        error={!!passwordConfirm.error}
-        errorText={passwordConfirm.error}
-        secureTextEntry={true}
-      />
-
-      <Text>Security Question</Text>
-      <Picker
-        style={[styles.picker]} itemStyle={styles.pickerItem}
-        onValueChange={(itemValue, itemIndex) =>
-          setSecurityQuestion({ value: itemValue, error: '' })
-        }>
-        <Picker.Item label="Mother's Maiden name" value="mdn" />
-        <Picker.Item label="Year of Birth" value="yob" />
-      </Picker>
-
-      <TextInput
-        label="Security Answer"
-        returnKeyType="next"
-        value={securityAnswer.value}
-        onChangeText={text => setSecurityAnswer({ value: text, error: '' })}
-        error={!!securityAnswer.error}
-        errorText={securityAnswer.error}
-      />
-
-      <Button mode="contained" onPress={_onSignUpPressed} style={styles.button}>
-        Sign Up
-      </Button>
-
-      <View style={styles.row}>
-        <Text style={styles.label}>Already have an account? </Text>
-        <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
-          <Text style={styles.link}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    </Background>
-  );
-};
+  render() {
+    return (
+      <Background>
+        {/* <BackButton goBack={() => navigation.navigate('HomeScreen')} /> */}
+  
+        <Logo />
+  
+        <Header>Create Account Armon</Header>
+  
+        <TextInput
+          label="Password"
+          returnKeyType="next"
+          value={this.state.password}
+          onChangeText={text => this.setState({password: text})}
+          // error={!!password.error}
+          // errorText={password.error}
+          secureTextEntry={true}
+        />
+  
+        {/* <TextInput
+          label="Paswword Confirm"
+          returnKeyType="next"
+          value={passwordConfirm.value}
+          onChangeText={text => setPasswordConfirm({ value: text, error: '' })}
+          error={!!passwordConfirm.error}
+          errorText={passwordConfirm.error}
+          secureTextEntry={true}
+        />
+  
+        <Text>Security Question</Text>
+        <Picker
+          style={[styles.picker]} itemStyle={styles.pickerItem}
+          onValueChange={(itemValue, itemIndex) =>
+            setSecurityQuestion({ value: itemValue, error: '' })
+          }>
+          <Picker.Item label="Mother's Maiden name" value="mdn" />
+          <Picker.Item label="Year of Birth" value="yob" />
+        </Picker>
+  
+        <TextInput
+          label="Security Answer"
+          returnKeyType="next"
+          value={securityAnswer.value}
+          onChangeText={text => setSecurityAnswer({ value: text, error: '' })}
+          error={!!securityAnswer.error}
+          errorText={securityAnswer.error}
+        /> */}
+  
+        <Button mode="contained" onPress={this._onSignUpPressed} style={styles.button}>
+          Sign Up
+        </Button>
+  
+        {/* <View style={styles.row}>
+          <Text style={styles.label}>Already have an account? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
+            <Text style={styles.link}>Login</Text>
+          </TouchableOpacity>
+        </View> */}
+      </Background>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   label: {
