29e0d8ca914439cf90c9475cdccf0cde4bcf24c0
unrealhound <armonkolaei@gmail.com>
2020-01-22 22:01:28 -0500

    + homescreen screen logic

--- wellBeingCheck/src/screens/HomeScreen.tsx
+++ wellBeingCheck/src/screens/HomeScreen.tsx
@@ -5,29 +5,74 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 import Paragraph from '../components/Paragraph';
 import { Navigation } from '../types';
+import { NavigationScreenProp, NavigationState, NavigationParams } from 'react-navigation';
+import { AsyncStorage, TouchableOpacity, StyleSheet } from 'react-native';
 
-type Props = {
-  navigation: Navigation;
-};
+type HomeState = {
+  canRegisterBtnIsDisabled: boolean,
+}
 
-const HomeScreen = ({ navigation, theme }: Props) => (
+interface Props {
+  navigation: NavigationScreenProp<NavigationState, NavigationParams>;
+}
 
-  <Background>
-    <Logo />
-    <Header>Well-Being</Header>
-    <Paragraph>
-      Statistics Canada - Well Being Survey
-    </Paragraph>
-    <Button mode="contained" onPress={() => navigation.navigate('LoginScreen')}>
-      Login
+class HomeScreen extends React.Component<Props, HomeState> {
+  constructor(RegisterState) {
+    super(RegisterState)
+    this.state = {
+      canRegisterBtnIsDisabled: false
+    };
+    this._accountAlreadyExists();
+  }
+
+  _accountAlreadyExists() {
+    const currentPassword = this._retrieveData('user_password');
+    if (!!currentPassword) {
+      //user already has account
+
+    }
+  }
+
+  _retrieveData = async (key) => {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value != null) {
+        return value;
+      }
+      else {
+        return "";
+      }
+    } catch (error) {
+    }
+  }
+
+  render() {
+    return (
+      <Background>
+        <Logo />
+        <Header>Well-Being</Header>
+        <Paragraph>
+          Statistics Canada - Well Being Survey
+        </Paragraph>
+        <Button mode="contained" onPress={() => this.props.navigation.navigate('LoginScreen')}>
+          Login
+        </Button>
+        <Button
+          style={styles.createButton}
+          mode="outlined"
+          onPress={() => this.props.navigation.navigate('RegisterScreen')}
+        >
+          Create Account
     </Button>
-    <Button
-      mode="outlined"
-      onPress={() => navigation.navigate('RegisterScreen')}
-    >
-      Sign Up
-    </Button>
-  </Background>
-);
+      </Background>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  createButton: {
+    opacity: 1,
+  },
+});
 
 export default memo(HomeScreen);
