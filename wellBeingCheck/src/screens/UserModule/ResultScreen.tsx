04519e3613312835cd4356809dc29164d68879f9
jiandong-guo <guojiandong@hotmail.com>
2020-02-03 21:40:47 -0500

    Move dashboard over

--- wellBeingCheck/src/screens/UserModule/ResultScreen.tsx
+++ wellBeingCheck/src/screens/UserModule/ResultScreen.tsx
@@ -1,43 +1,80 @@
-import React, { memo, useState, useCallback } from 'react';
-import { Picker, View, Text, StyleSheet, TouchableOpacity, Switch } from 'react-native';
-import { AsyncStorage } from 'react-native';
-import Background from '../../components/Background';
-import Logo from '../../components/Logo';
-import Header from '../../components/Header';
-import Button from '../../components/Button';
-import TextInput from '../../components/TextInput';
+import * as React from 'react';
+import { Text, View,ScrollView, StyleSheet,TouchableOpacity,Dimensions } from 'react-native';
+import TabNavigator from './TabPageScreen';
 import BackButton from '../../components/BackButton';
-import { newTheme } from '../../core/theme';
-import { List, Divider } from 'react-native-paper';
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
+export default class App extends React.Component {
+   constructor(props) {
+        super(props);
+        this.state = {
+          categes: [
+            { cat_id: '1', cat_name: 'Person', backgroundcolor: 'lightblue' },
+            { cat_id: '2', cat_name: 'Population', backgroundcolor: 'lightgray' },
+          ],
+          change: false,
+          isPopulation:false,loading: false,
+        };
+      }
+      changeBackground = item => {
+          let categes = JSON.parse(JSON.stringify(this.state.categes));
+          for (let x = 0; x < this.state.categes.length; x++) {
+            if (this.state.categes[x].cat_id == item.cat_id) {
+              categes[x].backgroundcolor = 'lightblue';
 
-import {
-  NavigationParams,
-  NavigationScreenProp,
-  NavigationState,
-} from 'react-navigation';
+            //  this.setState({categes: categes,});
+            } else {
+              categes[x].backgroundcolor = 'lightgray';
 
-interface Props {
-  navigation: NavigationScreenProp<NavigationState, NavigationParams>;
-}
+              //this.setState({categes: categes,});
+            }
+          }
+             this.setState({categes: categes,});
+             this.setState({isPopulation:item.cat_id=='2'});
+             console.log(this.state.isPopulation);
 
-class ResultScreen extends React.Component<Props> {
+        };
+        displayTab() {
+              //  this.setState({loading: true});
+
+                if (this.state.isPopulation) {
+                    return <Text> The survey is not done yet,please check resule after 6 months! </Text>;
+                } else {
+                      console.log('isPopulation:'+this.state.isPopulation);
+                    return <TabNavigator />;
+
+                }
+            }
   render() {
     return (
-      <View>
-        <BackButton goBack={() => this.props.navigation.navigate('Dashboard')} />
-        <View style={styles.content}>
-            <Text style={{ fontSize: 30 }}>Result Screen</Text>
+      <View style={styles.container}>
+        <View style={{flexDirection:'row',justifyContent: 'space-around'}}>
+                             {this.state.categes.map((item, key) => (
+                                       <TouchableOpacity key={key}
+                                         style={{
+                                           width: deviceWidth/2,
+                                           height: 50,
+                                           alignItems: 'center',
+                                           justifyContent: 'center',
+                                           backgroundColor: item.backgroundcolor,
+                                         }}
+                                         onPress={() => this.changeBackground(item)}>
+                                         <Text style={{ color: 'black',fontSize:13 }}>
+
+                                           {item.cat_name.toUpperCase()}
+                                         </Text>
+                                       </TouchableOpacity>
+                                     ))}
         </View>
-      </View>
+         {this.displayTab()}
+</View>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  content: {
-    alignItems: 'center',
-    marginTop: 100
-  }
+  container: {
+    flex:1,
+    marginTop:24,// Constants.statusBarHeight,
+    backgroundColor: 'white',
+  },
 });
-
-export default memo(ResultScreen);
