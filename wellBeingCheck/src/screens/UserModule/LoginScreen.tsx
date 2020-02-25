7f9306c016fdb200df03a586451cf9c1982cc804
unrealhound <armonkolaei@gmail.com>
2020-02-24 19:11:22 -0500

    forgot password ui

--- wellBeingCheck/src/screens/UserModule/LoginScreen.tsx
+++ wellBeingCheck/src/screens/UserModule/LoginScreen.tsx
@@ -104,12 +104,6 @@ class LoginScreen extends React.Component<Props, LoginState> {
             <Text style={styles.whiteText}>Login</Text>
           </Button>
 
-          {/* <View style={styles.row}>
-          <Text style={styles.label}>Don’t have an account? </Text>
-          <TouchableOpacity onPress={() => this.props.navigation.navigate('RegisterScreen')}>
-            <Text style={styles.link}>Sign up</Text>
-          </TouchableOpacity>
-        </View> */}
         </Background>
       </PaperProvider>
     );
