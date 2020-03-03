81950d0744ce8343a430914e624de7127456b631
Thierry Ramanampanoharana <thierry.ramanampanoharana@canada.ca>
2020-03-02 21:44:16 -0500

    wellBeingCheck-63 Secured WebAPI Added short-uuid to generate deviceId and then add a 2-digit chech to it References #63

--- wellBeingCheck/src/api/device-id.service.spec.ts
+++ wellBeingCheck/src/api/device-id.service.spec.ts
@@ -1,6 +1,6 @@
 import * as DeviceIdService from './device-id.service';
 
-test('The generated deviceId is not empty', () => {
+test('The generated deviceId is not empty and does not exceed 22 in length', () => {
    let deviceId: string = DeviceIdService.generateNewDeviceId();
 
    // Not null
@@ -8,4 +8,21 @@ test('The generated deviceId is not empty', () => {
 
    // Not empty
    expect((/^\s*$/).test(deviceId)).toBeFalsy();
+
+   expect(deviceId.length).toBeLessThanOrEqual(24);
+});
+
+test('The generated deviceId is correct', () => {
+   let deviceId: string = DeviceIdService.generateNewDeviceId();
+
+   // Not null
+   expect(deviceId).not.toBeNull();
+
+   // valid with checksum
+   expect(DeviceIdService.isValidDeviceId(deviceId)).toBeTruthy();
+});
+
+test('Flag invalid device id', () => {
+   // Not empty
+   expect(DeviceIdService.isValidDeviceId('myIphone')).toBeFalsy();
 });
--- wellBeingCheck/src/api/device-id.service.ts
+++ wellBeingCheck/src/api/device-id.service.ts
@@ -1,10 +1,14 @@
+import * as ShortUUID from 'short-uuid';
+import {Modulus2833Radix62} from './modulo-check-scheme';
+
 /**
  * Generates a new device Id with two digits checksum.
  * The generated device Id should not exceed 20 characters in total.
  * @return a string not longer than 20 characters. Each character belongs to [0-9a-zA-Z]
  */
 export function generateNewDeviceId(): string {
-    return "";
+    let shortUuid = ShortUUID.generate();
+    return Modulus2833Radix62.generateFullCheckedForm(shortUuid);
 }
 
 /**
@@ -14,5 +18,5 @@ export function generateNewDeviceId(): string {
  * @return true if the device Id and the checksum match
  */
 export function isValidDeviceId(deviceId: string): boolean {
-    return false;
+    return Modulus2833Radix62.isCorrect(deviceId);
 }
