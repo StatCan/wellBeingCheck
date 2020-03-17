7cd19d8087a349d2a42a25bce91cd012f602b676
jiandong-guo <guojiandong@hotmail.com>
2020-03-17 11:35:41 -0400

    Update ResultScreen.tsx

--- wellBeingCheck/src/screens/UserModule/ResultScreen.tsx
+++ wellBeingCheck/src/screens/UserModule/ResultScreen.tsx
@@ -97,7 +97,7 @@ export default class App extends React.Component<Props,ScreenState> {
       let xd=index*width1;
       this.setState({current:index});
       if(index==0){this.setState({title:resources.getString("Your feelings")});}
-      else if(index==1){this.setState({title:resources.getString("HHow you are feeling by location")});}
+      else if(index==1){this.setState({title:resources.getString("How you are feeling by location")});}
       else if(index==2){this.setState({title:resources.getString("How you are feeling with others")});}
       else if(index==3){this.setState({title:resources.getString("How you are feeling by activity")});}
       InteractionManager.runAfterInteractions(()=>this.sv.scrollTo({x:xd}))
