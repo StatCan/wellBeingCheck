641f0a69a9ce971f328c3cc0ea2d13d06f0e6f6b
jiandong-guo <guojiandong@hotmail.com>
2020-04-03 09:37:54 -0400

    temp

--- wellBeingCheck/src/screens/UserModule/EQSurveyScreen.tsx
+++ wellBeingCheck/src/screens/UserModule/EQSurveyScreen.tsx
@@ -45,7 +45,7 @@ export default class EQSurveyScreen extends React.Component<Props, ScreenState> 
     //   this.resetPasswordNew('Esm#12346789');
 
    }
-   resetWebView(){
+       resetWebView(){
         let disCode= 'const meta = document.createElement("meta"); meta.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"); meta.setAttribute("name", "viewport"); document.getElementsByTagName("head")[0].appendChild(meta);';
            let clearCookie='document.cookie.split(";").forEach(function(c) {document.cookie = c.trim().split("=")[0] + "=;" + "expires=Thu, 01 Jan 1970 00:00:00 UTC;";});';
            let jsCode=clearCookie+'document.addEventListener("message", function (message) { document.getElementById("langtest").click(); });var btn = document.createElement("button");btn.style.visibility ="hidden";btn.onclick = switchlang;btn.setAttribute("id", "langtest");document.body.appendChild(btn);    function switchlang() { var a = document.querySelector("a.sc-js-langchange");var href = a.href;if (href.indexOf("/q/fr")>0) {var res = href.replace("/q/fr", "/q/en");a.setAttribute("href", res);a.click();} else if (href.indexOf("/q/en")>0) {var res = href.replace("/q/en", "/q/fr");a.setAttribute("href", res);a.click();} }';
@@ -180,7 +180,7 @@ export default class EQSurveyScreen extends React.Component<Props, ScreenState> 
                    //  .then((responseJson) => {console.log('resetPassword:'+responseJson);    return responseJson;})
                     .catch((error) => {console.error(error);console.log('Bad');return false;});
                }
-   async resetPasswordNew(newPass){
+      async resetPasswordNew(newPass){
            let service = new BackEndService(
                  WEB_API_BASE_URL,
                  'fr-CA',
