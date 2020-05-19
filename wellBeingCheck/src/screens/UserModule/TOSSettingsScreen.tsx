74f2a13d89a35f32b335370e6a123e4ce3d0eb5c
AJBouzidi <58231743+AJBouzidi@users.noreply.github.com>
2020-05-19 00:26:12 -0400

    Update TOSSettingsScreen.tsx
    fixing the Term of Service font and color according to the UX specs

--- wellBeingCheck/src/screens/UserModule/TOSSettingsScreen.tsx
+++ wellBeingCheck/src/screens/UserModule/TOSSettingsScreen.tsx
@@ -50,11 +50,11 @@ class TermsOfServiceScreen extends React.Component<Props, TermsOfServiceState> {
       <PaperProvider theme={newTheme}>
         <SafeAreaConsumer>{insets => <View style={{ paddingTop: insets.top }} />}</SafeAreaConsumer>
         <AppBanner />
-          <SafeAreaView style={styles.container}>
-            <ScrollView style={styles.scrollView}>
-              <LogoClearSmall />
-              <Title style={styles.title}>{resources.getString("terms_and_conditions")}</Title>
-              <Paragraph style={styles.paragraph}>
+        <SafeAreaView style={styles.container}>
+          <ScrollView style={styles.scrollView}>
+            <LogoClearSmall />
+            <Title style={styles.title}>{resources.getString("terms_and_conditions")}</Title>
+            <Paragraph style={styles.paragraph}>
               <ParsedText
                 style={styles.text}
                 parse={
@@ -72,16 +72,26 @@ class TermsOfServiceScreen extends React.Component<Props, TermsOfServiceState> {
                     { pattern: /Loi sur les langues officielles/, style: styles.italic },
                     { pattern: /Norme sur l’optimisation des sites Web et des applications pour appareils mobiles/, style: styles.italic },
                     { pattern: /Loi sur le droit d'auteur du Canada/, style: styles.italic },
-                    {type: 'url',                       style: styles.url, onPress: this.handleUrlPress},
+
+                    { pattern: /Disclaimer  |General terms and conditions|Modification of Terms and Conditions|Privacy notice/, style: styles.bold },
+                    { pattern: /Confidentiality statement| Confidentiality statement|Official language notice|Accessibility notice/, style: styles.bold },
+                    { pattern: /Use of content|Unique identifier|Applicable law  |Limitation of liability/, style: styles.bold },
+                    { pattern: /Disclosure statement|Copyright notice |Trademark notice|No warranties|Indemnity|Restrictions|Modifications and access|Ownership|Maintenance and support/, style: styles.bold },
+
+                    { pattern: /Désistement |Conditions générales  |Modification des conditions générales|Avis de confidentialité/, style: styles.bold },
+                    { pattern: /Énoncé de confidentialité|Langues officielles  |Avis de l’accessibilité|Utilisation du contenu/, style: styles.bold },
+                    { pattern: /Use of content|Code d’identification unique |Lois applicables |Limitation de la responsabilité/, style: styles.bold },
+                    { pattern: /Avis de divulgations|Droit d’auteur |Avis concernant l'image de marque|Aucune garantie|Indemnité|Restrictions|Modification et accès|Propriété|Maintenance et soutien/, style: styles.bold },
+                    { type: 'url', style: styles.url, onPress: this.handleUrlPress },
                   ]
                 }
                 childrenProps={{ allowFontScaling: false }}
               >
-                 {resources.getString("terms_and_conditions_content")}
+                {resources.getString("terms_and_conditions_content")}
               </ParsedText>
-              </Paragraph>
-            </ScrollView>
-          </SafeAreaView>
+            </Paragraph>
+          </ScrollView>
+        </SafeAreaView>
         <Button style={styles.btnNext}
           mode="contained"
           onPress={this._openSettingsScreen}>
@@ -114,6 +124,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 22,
     fontWeight: 'bold',
+    color: '#656262',
   },
   label: {
     color: newTheme.colors.secondary,
@@ -137,9 +148,17 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
   },
   text: {
+    color: '#656262',
+
   },
   italic: {
     fontStyle: 'italic',
+  },
+  bold: {
+    fontWeight: 'bold',
+    color: '#66cc99',
+    //fontFamily: 'Lato-Bold',
+    fontSize: 18
   },
   url: {
     color: 'blue',
