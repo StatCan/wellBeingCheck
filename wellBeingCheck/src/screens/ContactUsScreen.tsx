75a66f4fa4f1b6c5ccd5cb1d96a238f3d2976de6
Forum Gorawala <84411852+ForumG-SC@users.noreply.github.com>
2021-10-05 15:33:53 -0400

    #355 Contact info update

--- wellBeingCheck/src/screens/ContactUsScreen.tsx
+++ wellBeingCheck/src/screens/ContactUsScreen.tsx
@@ -1,141 +1,204 @@
-import React, { memo } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Linking, SafeAreaView, ScrollView, PanResponder, Alert, BackHandler, Image } from 'react-native';
-import Button from '../components/Button';
-import { Provider as PaperProvider, Title } from 'react-native-paper';
-import { newTheme } from '../core/theme';
-import AppBanner from '../components/AppBanner';
-import LogoClearSmall from '../components/LogoClearSmall';
-import { resources } from '../../GlobalResources';
-import { NavigationParams, NavigationScreenProp, NavigationState } from 'react-navigation';
-import BackgroundWhite from '../components/BackgroundWhite';
-import { SafeAreaConsumer } from 'react-native-safe-area-context';
-import { Updates } from 'expo';
-import { sendDelayedNotification} from '../utils/schedule';
+/** @format */
+
+import React, { memo } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Linking,
+  SafeAreaView,
+  ScrollView,
+  PanResponder,
+  Alert,
+  BackHandler,
+  Image,
+} from "react-native";
+import Button from "../components/Button";
+import { Provider as PaperProvider, Title } from "react-native-paper";
+import { newTheme } from "../core/theme";
+import AppBanner from "../components/AppBanner";
+import LogoClearSmall from "../components/LogoClearSmall";
+import { resources } from "../../GlobalResources";
+import {
+  NavigationParams,
+  NavigationScreenProp,
+  NavigationState,
+} from "react-navigation";
+import BackgroundWhite from "../components/BackgroundWhite";
+import { SafeAreaConsumer } from "react-native-safe-area-context";
+import { Updates } from "expo";
+import { sendDelayedNotification } from "../utils/schedule";
 
 interface Props {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }
 
 class ContactUsScreen extends React.Component<Props> {
-
   backHandler: any;
 
   constructor(Props) {
-    super(Props)
+    super(Props);
   }
 
   componentDidMount() {
-    this.backHandler = BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+    this.backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      this.handleBackPress
+    );
   }
 
   componentWillUnmount() {
-    this.backHandler.remove()
+    this.backHandler.remove();
   }
 
   handleBackPress = () => {
     return true;
-  }
+  };
   _onNextBtnHandle = () => {
-   global.resetTimer();//  global.globalTick=0;
-    this.props.navigation.navigate('Dashboard');
-  }
-printLogo=()=>{
-        alert('Scheduled:\r\n'+global.sendouts+'\r\nIndicator:'+global.busy+'\r\nUserId:'+global.userToken+'\r\nSac:'+global.sac);
- }
- printLog=()=>{
-         alert('Sys Log:\r\n'+global.syslog);
-  }
- printTest=()=>{
-        sendDelayedNotification(new Date(),'Test Notification','When you see it, your setting is good');
-  }
+    global.resetTimer(); //  global.globalTick=0;
+    this.props.navigation.navigate("Dashboard");
+  };
+  printLogo = () => {
+    alert(
+      "Scheduled:\r\n" +
+        global.sendouts +
+        "\r\nIndicator:" +
+        global.busy +
+        "\r\nUserId:" +
+        global.userToken +
+        "\r\nSac:" +
+        global.sac
+    );
+  };
+  printLog = () => {
+    alert("Sys Log:\r\n" + global.syslog);
+  };
+  printTest = () => {
+    sendDelayedNotification(
+      new Date(),
+      "Test Notification",
+      "When you see it, your setting is good"
+    );
+  };
   render() {
     return (
       <PaperProvider theme={newTheme}>
-        <SafeAreaConsumer>{insets => <View style={{ paddingTop: insets.top }} />}</SafeAreaConsumer>
+        <SafeAreaConsumer>
+          {(insets) => <View style={{ paddingTop: insets.top }} />}
+        </SafeAreaConsumer>
         <AppBanner />
         <BackgroundWhite>
           <SafeAreaView style={styles.container}>
-            <ScrollView style={styles.scrollView}
-               {...global.panResponder.panHandlers}
-            >
-              <View style={styles.logo_container}
-                {...global.panResponder.panHandlers}
-               >
-                {/* <LogoClearSmall /> */}
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('Dashboard')}
-                            style={{ marginLeft: 5, marginTop: 10,marginBottom:5 }}>
-                <Image source={require('../assets/WellnessCheckLogo.png')}
-                       style={{ width: 38, height: 38 }} />
-            </TouchableOpacity>
-              </View>
-              <Title style={styles.title}
+            <ScrollView
+              style={styles.scrollView}
               {...global.panResponder.panHandlers}
+            >
+              <View
+                style={styles.logo_container}
+                {...global.panResponder.panHandlers}
               >
-                {resources.getString("contactus_title")}</Title>
+                {/* <LogoClearSmall /> */}
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate("Dashboard")}
+                  style={{ marginLeft: 5, marginTop: 10, marginBottom: 5 }}
+                >
+                  <Image
+                    source={require("../assets/WellnessCheckLogo.png")}
+                    style={{ width: 38, height: 38 }}
+                  />
+                </TouchableOpacity>
+              </View>
+              <Title style={styles.title} {...global.panResponder.panHandlers}>
+                {resources.getString("contactus_title")}
+              </Title>
               <View>
                 <View style={styles.content}>
-                  <Text style={styles.content_title}
-                  accessible={true}
-                  accessibilityRole='header'
-                  accessibilityLabel={resources.getString("contactus_email")}
-                  >{resources.getString("contactus_email")}</Text>
-                  <TouchableOpacity onPress={() => Linking.openURL('mailto:infostats@canada.ca')}>
-                    <Text
-                    style={styles.text}
-                    >infostats@canada.ca</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.content}>
-                  <Text style={styles.content_title}
-                  accessible={true}
-                  accessibilityRole='header'
-                  accessibilityLabel={resources.getString("contactus_telephone")}
-                  >{resources.getString("contactus_telephone")}</Text>
-                  <TouchableOpacity onPress={() => Linking.openURL('tel:18779499492')}>
-                    <Text style={styles.text}>1-877-949-9492</Text></TouchableOpacity>
-                </View>
-                <View style={styles.content}>
-                  <Text style={styles.content_title}
-                   accessible={true}
-                   accessibilityRole='header'
-                   accessibilityLabel={resources.getString("contactus_website")}
-                  >{resources.getString("contactus_website")}</Text>
-                  <TouchableOpacity onPress={() => Linking.openURL('https://www.statcan.gc.ca')}
-
+                  <Text
+                    style={styles.content_title}
+                    accessible={true}
+                    accessibilityRole="header"
+                    accessibilityLabel={resources.getString("contactus_email")}
                   >
-                    <Text style={styles.text}
-                      accessible={true}
-                      accessibilityRole='link'
-                      accessibilityLabel='https://www.statcan.gc.ca'
-                    >https://www.statcan.gc.ca</Text>
+                    {resources.getString("contactus_email")}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() =>
+                      Linking.openURL("mailto:infostats@statcan.gc.ca")
+                    }
+                  >
+                    <Text style={styles.text}>infostats@statcan.gc.ca</Text>
                   </TouchableOpacity>
                 </View>
                 <View style={styles.content}>
-                  <Text style={styles.content_title}
-                  accessible={true}
-                  accessibilityRole='header'
-                  accessibilityLabel={resources.getString("contactus_mail")}
-                  >{resources.getString("contactus_mail")}</Text>
-                  <Text style={styles.text}>{resources.getString("contactus_text")}
+                  <Text
+                    style={styles.content_title}
+                    accessible={true}
+                    accessibilityRole="header"
+                    accessibilityLabel={resources.getString(
+                      "contactus_telephone"
+                    )}
+                  >
+                    {resources.getString("contactus_telephone")}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => Linking.openURL("tel:18779499492")}
+                  >
+                    <Text style={styles.text}>1-877-949-9492</Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.content}>
+                  <Text
+                    style={styles.content_title}
+                    accessible={true}
+                    accessibilityRole="header"
+                    accessibilityLabel={resources.getString(
+                      "contactus_website"
+                    )}
+                  >
+                    {resources.getString("contactus_website")}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => Linking.openURL("https://www.statcan.gc.ca")}
+                  >
+                    <Text
+                      style={styles.text}
+                      accessible={true}
+                      accessibilityRole="link"
+                      accessibilityLabel="https://www.statcan.gc.ca"
+                    >
+                      https://www.statcan.gc.ca
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.content}>
+                  <Text
+                    style={styles.content_title}
+                    accessible={true}
+                    accessibilityRole="header"
+                    accessibilityLabel={resources.getString("contactus_mail")}
+                  >
+                    {resources.getString("contactus_mail")}
+                  </Text>
+                  <Text style={styles.text}>
+                    {resources.getString("contactus_text")}
                   </Text>
                 </View>
-
-
-
-
-
-
               </View>
             </ScrollView>
           </SafeAreaView>
         </BackgroundWhite>
-        <Button style={styles.btnNext}
-            mode="contained"
-            onPress={this._onNextBtnHandle}>
-            <Text style={styles.btnText}>{resources.getString("gl.return")}</Text>
-         </Button>
-        <SafeAreaConsumer>{insets => <View style={{ paddingTop: insets.top }} />}</SafeAreaConsumer>
-      </PaperProvider >
+        <Button
+          style={styles.btnNext}
+          mode="contained"
+          onPress={this._onNextBtnHandle}
+        >
+          <Text style={styles.btnText}>{resources.getString("gl.return")}</Text>
+        </Button>
+        <SafeAreaConsumer>
+          {(insets) => <View style={{ paddingTop: insets.top }} />}
+        </SafeAreaConsumer>
+      </PaperProvider>
     );
   }
 }
@@ -146,49 +209,49 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   logo_container: {
-    position: 'relative',
+    position: "relative",
     marginTop: 10,
     marginLeft: 10,
   },
   title: {
     marginTop: 10,
     marginLeft: 20,
-    fontWeight: '900',
+    fontWeight: "900",
     fontSize: 30,
-    color: '#707070',
+    color: "#707070",
     marginBottom: 20,
   },
   content_title: {
-    color: '#66cc99',
+    color: "#66cc99",
     fontSize: 18,
-    fontWeight:'bold'
+    fontWeight: "bold",
   },
   btnNext: {
     color: newTheme.colors.whiteText,
-    width: 100, height: 40,
+    width: 100,
+    height: 40,
     alignSelf: "flex-end",
     marginRight: 20,
-    marginBottom: 4, marginTop: 4
+    marginBottom: 4,
+    marginTop: 4,
   },
   btnText: {
     color: newTheme.colors.whiteText,
   },
   container: {
-    width: '100%',
-    height: '90%'
+    width: "100%",
+    height: "90%",
   },
   scrollView: {
-    width: '100%',
+    width: "100%",
   },
-  text:{
-    fontSize:15,
-    color: '#000',
-  }
+  text: {
+    fontSize: 15,
+    color: "#000",
+  },
 });
 
 export default memo(ContactUsScreen);
-
-
 
 //printLog=()=>{
 //      let list=global.schedules;let str='Calculated:\r\n';
@@ -203,7 +266,6 @@ export default memo(ContactUsScreen);
 //  printLog1=()=>{
 //       alert('Received:\r\n'+global.received);
 //  }
-
 
 //  <Button style={styles.btnNext}
 //                  mode="contained"
@@ -249,9 +311,7 @@ export default memo(ContactUsScreen);
                                <Text style={styles.btnText}>Test</Text>
                   </Button>*/
 
-
-
- /*<View style={styles.content}>
+/*<View style={styles.content}>
                                   <Text style={styles.content_title}
                                    accessible={true}
                                    accessibilityRole='header'
@@ -268,9 +328,7 @@ export default memo(ContactUsScreen);
                                   </TouchableOpacity>
                                 </View>*/
 
-
-
-                           /*     <Button style={styles.btnNext}
+/*     <Button style={styles.btnNext}
                                           mode="contained"
                                           onPress={this._onNextBtnHandle}>
                                           <Text style={styles.btnText}>{resources.getString("gl.return")}</Text>
